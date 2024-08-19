6e6726fd9420d5696574640931aa3f97938031a1
Usuario <Usuario@CPD02-M20>
2024-08-19 15:33:34 -0300

    ButtonConfirmacao

--- MauiAppShowDoMilhao/MainPage.xaml.cs
+++ MauiAppShowDoMilhao/MainPage.xaml.cs
@@ -9,6 +9,58 @@
             InitializeComponent();
         }
 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            this.BindingContext = App.getRandomPerguntaFacil();
+        }
+
+        private void Button_Clicked_Proxima(object sender, EventArgs e)
+        {
+            bool acertou = false;
+            string resp = "";
+            bool valor;
+
+            if (alt0.IsChecked)
+            {
+                if ((bool)alt0.Value)
+                {
+                    acertou = true;
+                    resp = alt0.Content.ToString();
+                }
+            }
+
+            if (alt1.IsChecked)
+            {
+                if ((bool)alt1.Value)
+                {
+                    acertou = true;
+                    resp = alt1.Content.ToString();
+                }
+            }
+            if (alt2.IsChecked)
+            {
+                if ((bool)alt2.Value)
+                {
+                    acertou = true;
+                    resp = alt2.Content.ToString();
+                }
+            }
+            if (alt3.IsChecked)
+            {
+                if ((bool)alt3.Value)
+                {
+                    acertou = true;
+                    resp = alt3.Content.ToString();
+                }
+            }
+
+            if (acertou)
+            {
+                DisplayAlert("ACERTOU!", resp, "OK");
+                this.BindingContext = App.getRandomPerguntaFacil();
+            }
+        }
+
         private void OnCounterClicked(object sender, EventArgs e)
         {
             count++;
